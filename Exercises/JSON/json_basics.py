855175f9d58d55290fbe5de08212503a2f9909a5
andreiboa <andreiboa135@gmail.com>
2026-09-02 22:45:37 +0800

    python variable like JSON

---
+++ Exercises/JSON/json_basics.py
@@ -0,0 +1,19 @@
+person = {
+    "name": "Andrei Boa",
+    "age": 22,
+    "learning_ai_ml": True,
+    "skills": [
+        "Python",
+        "Java",
+        "C#"
+    ],
+    "Tools": {
+        "engine": "Unity",
+        "IDE": [
+            "Visual Studio",
+            "Netbeans"
+        ]
+    }
+}
+
+print(person)
